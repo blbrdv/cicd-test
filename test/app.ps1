a596bcf7213ac7c3985440cef352b48bcfd0b36b
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-06-11 12:11:35 +0700

    Fix path

--- test/app.ps1
+++ test/app.ps1
@@ -1,9 +1,9 @@
 function Run {
 
-    $Result = & tree test 2>&1;
+    $Result = & tree .\test 2>&1;
 
     if ( $LastExitCode -ne 0 ) {
-        throw "app exit with code ${LastExitCode}";
+        throw "App exit with code ${LastExitCode}";
     }
 
     return $Result.Split("\n");
