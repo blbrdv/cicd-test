2968b62d03042456b729d2cc43a84de93a94d02c
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-06-11 10:22:47 +0700

    Update test

--- test.ps1
+++ test.ps1
@@ -13,26 +13,26 @@ $InstallResult = Install $ID $Version;
 
 Test
     @(
-        '[DEB] Trace file: .:[^.]\.log',
-        '[DEB] Temp dir: .:\Users\[^\]\AppData\Local\Temp\ezstore\' + [regex]::Escape($ID),
-        '[DEB] Fetching cookie\.\.\.',
-        '[INF] Cookie fetched',
-        '[DEB] Fetching product info\.\.\.',
-        '[INF] Product info fetched',
-        '[DEB] Fetching product files\.\.\.',
-        '[INF] Product files fetched',
-        '[DEB] Download product files\.\.\.',
-        '[INF] Product files downloaded',
-        '[INF] Package ' + [regex]::Escape($Name) + ' ' + [regex]::Escape($Version) + ' installed.',
-        '[SCC] Done!'
+        '^[DEB] Trace file: .:[^.]\.log$',
+        '^[DEB] Temp dir: .:\Users\[^\]\AppData\Local\Temp\ezstore\' + [regex]::Escape($ID) + '$',
+        '^[DEB] Fetching cookie\.\.\.$',
+        '^THIS MUST FAIL$' #'^[INF] Cookie fetched$',
+        '^[DEB] Fetching product info\.\.\.$',
+        '^[INF] Product info fetched$',
+        '^[DEB] Fetching product files\.\.\.$',
+        '^[INF] Product files fetched$',
+        '^[DEB] Download product files\.\.\.$',
+        '^[INF] Product files downloaded$',
+        '^[INF] Package ' + [regex]::Escape($Name) + ' ' + [regex]::Escape($Version) + ' installed.$',
+        '^[SCC] Done!$'
     ), $InstallResult;
 
-$AppResult = App;
+$AppResult = Run;
 
 Test
     @(
-        'Folder PATH listing for volume .*'
-        'Volume serial number is .*'
-        '.:.*'
-        'No subfolders exist '
+        '^Folder PATH listing for volume .*$'
+        '^Volume serial number is .*$'
+        '^.:.*$'
+        '^No subfolders exist $'
     ), $AppResult;
