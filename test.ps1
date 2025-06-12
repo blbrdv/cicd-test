e812bf9b0eff53665585d33c6be7a924030414c1
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-06-12 11:41:47 +0700

    Add path out

--- test.ps1
+++ test.ps1
@@ -6,15 +6,4 @@ trap { Write-Error $_ -ErrorAction Continue; exit 1 };
 
 Write-Host "======";
 
-$U = @(
-    'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*',
-    'HKLM:\SOFTWARE\Wow6432node\Microsoft\Windows\CurrentVersion\Uninstall\*',
-    'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*',
-    'HKCU:\SOFTWARE\Wow6432node\Microsoft\Windows\CurrentVersion\Uninstall\*'
-);
-
-foreach ($UKey in $U) {
-    foreach ($Product in (Get-ItemProperty $UKey -ErrorAction SilentlyContinue)) {
-        $Product | Format-Table -AutoSize;
-    }
-};
+gci env:Path | %{$_.Value.Split(';')}
