fd6e57d730cef090626cd922abab9ccb2d91b2cb
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-06-12 10:51:42 +0700

    Fix displayname

--- test.ps1
+++ test.ps1
@@ -4,7 +4,10 @@ trap { Write-Error $_ -ErrorAction Continue; exit 1 };
 
 .\ezstore.exe install 9mvsm3j7zj7c --ver v1.1.0.0 --verbosity d
 
-Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall
-    | foreach-object {Get-ItemProperty $_.PsPath}
-    | %{$_.Displayname}
-    | echo
+$Apps = Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall | foreach-object {Get-ItemProperty $_.PsPath}
+
+for ($App in $Apps) {
+    if ( [bool]($App -match "Displayname") ) {
+        echo $App.Displayname;
+    }
+}
