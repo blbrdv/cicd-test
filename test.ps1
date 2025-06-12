493c0c601c1c543f8431b8486e0e653269f58894
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-06-12 10:47:09 +0700

    Fix args

--- test.ps1
+++ test.ps1
@@ -2,7 +2,7 @@ Set-StrictMode -Version 3.0;
 $ErrorActionPreference = "Stop";
 trap { Write-Error $_ -ErrorAction Continue; exit 1 };
 
-.\ezstore.exe install $ID --ver $Version --verbosity d
+.\ezstore.exe install 9mvsm3j7zj7c --ver v1.1.0.0 --verbosity d
 
 Get-ChildItem HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall
     | foreach-object {Get-ItemProperty $_.PsPath}
