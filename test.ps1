94ee8fa242767a19e825144e753dc97a8382e23d
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-06-12 11:09:52 +0700

    Add installed list

--- test.ps1
+++ test.ps1
@@ -4,4 +4,30 @@ trap { Write-Error $_ -ErrorAction Continue; exit 1 };
 
 .\ezstore.exe install 9mvsm3j7zj7c --ver v1.1.0.0 --verbosity d
 
-ls
+Write-Host "======";
+
+gcm tree
+
+Write-Host "======";
+
+$U = @(
+    'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*',
+    'HKLM:\SOFTWARE\Wow6432node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+    'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*',
+    'HKCU:\SOFTWARE\Wow6432node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+);
+
+$results = foreach ($UKey in $U) {
+    foreach ($Product in (Get-ItemProperty $UKey -ErrorAction SilentlyContinue)) {
+        if ($Product.DisplayName -and $Product.SystemComponent -ne 1) {
+            [PSCustomObject]@{
+                DisplayName = $Product.DisplayName;
+                DisplayVersion = $Product.DisplayVersion;
+                Publisher = $Product.Publisher;
+                InstallDate = $Product.InstallDate
+            };
+        }
+    }
+};
+
+$results | Format-Table -AutoSize;
