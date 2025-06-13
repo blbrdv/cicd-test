adf9bda94c7085bdc427cb0ac683d5d83c19d30c
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-06-13 18:10:04 +0700

    Fix expected veriable

--- tests/not-found.ps1
+++ tests/not-found.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop";
 trap { Write-Error $_ -ErrorAction Continue; exit 1 };
 
 $Id = "f1o2o3b4a5r6";
-$Expected = '^\[ERR\] Finished with error: can not fetch product info: product with id "' $Id '" and locale "en-US" not found$';
+$Expected = '^\[ERR\] Finished with error: can not fetch product info: product with id "' + $Id + '" and locale "en-US" not found$';
 
 Write-Host "";
 $Actual = .\bin\ezstore.exe install $Id --verbosity m 2>&1;
