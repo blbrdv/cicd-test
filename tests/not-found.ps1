03e682e16c0dce8f481bedaf1e5a09e7a1657861
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-06-13 16:25:03 +0700

    Fix test

--- tests/not-found.ps1
+++ tests/not-found.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop";
 trap { Write-Error $_ -ErrorAction Continue; exit 1 };
 
 Write-Host "";
-$Expected = '\[ERR\] Finished with error: can not fetch product info: product with id "f1o2o3b4a5r6" and locale "en-US" not found';
+$Expected = '^\[ERR\] Finished with error: can not fetch product info: product with id "f1o2o3b4a5r6" and locale "en-US" not found$';
 $Actual = .\bin\ezstore.exe install f1o2o3b4a5r6 --verbosity m 2>&1;
 Write-Host $Actual;
 $Actual = $Actual -replace '\x1b\[[0-9;]*m';
@@ -14,5 +14,5 @@ if ( $Actual -match $Expected ) {
     exit 0;
 }
 
-Write-Host: "Expected: '$Expected'";
+Write-Host "Expected (regexp): '$Expected'";
 throw 'Incorrect app output.';
