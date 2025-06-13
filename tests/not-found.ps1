800761cdc6d2884afcff113f00a6cb617a5cef03
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-06-13 18:05:27 +0700

    Refactoring

--- tests/not-found.ps1
+++ tests/not-found.ps1
@@ -2,9 +2,11 @@ Set-StrictMode -Version 3.0;
 $ErrorActionPreference = "Stop";
 trap { Write-Error $_ -ErrorAction Continue; exit 1 };
 
+$Id = "f1o2o3b4a5r6";
+$Expected = '^\[ERR\] Finished with error: can not fetch product info: product with id "' $Id '" and locale "en-US" not found$';
+
 Write-Host "";
-$Expected = '^\[ERR\] Finished with error: can not fetch product info: product with id "f1o2o3b4a5r6" and locale "en-US" not found$';
-$Actual = .\bin\ezstore.exe install f1o2o3b4a5r6 --verbosity m 2>&1;
+$Actual = .\bin\ezstore.exe install $Id --verbosity m 2>&1;
 Write-Host $Actual;
 $Actual = $Actual -replace '\x1b\[[0-9;]*m';
 
