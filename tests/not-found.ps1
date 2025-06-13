9903a6d48186407d4f8234c1dbd64da005f29ecc
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-06-13 16:05:04 +0700

    Add not-found test

--- tests/not-found.ps1
+++ tests/not-found.ps1
@@ -1,1 +1,21 @@
-Write-Host "Not found";
+Set-StrictMode -Version 3.0;
+$ErrorActionPreference = "Stop";
+trap { Write-Error $_ -ErrorAction Continue; exit 1 };
+
+Write-Host "";
+$Expected = '';
+$Actual = .\bin\ezstore.exe install f1o2o3b4a5r6 --verbosity m 2>&1;
+$Actual = $Actual -replace '\x1b\[[0-9;]*m';
+
+Write-Host "";
+
+
+if ( $Actual -match $Expected ) {
+    Write-Host "Test passed!";
+    exit 0;
+}
+
+Write-Host: "Expected: '$Expected'";
+Write-Host: "Actual:";
+Write-Host: $Actual;
+throw 'Incorrect app output.';
