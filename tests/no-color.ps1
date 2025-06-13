74a264e593c570a48b4c6bda111c179e203c6fe1
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-06-13 16:09:21 +0700

    Add no-color test

--- tests/no-color.ps1
+++ tests/no-color.ps1
@@ -1,1 +1,21 @@
-Write-Host "No color";
+Set-StrictMode -Version 3.0;
+$ErrorActionPreference = "Stop";
+trap { Write-Error $_ -ErrorAction Continue; exit 1 };
+
+$Id = "9mvsm3j7zj7c";
+$Name = "PeterEtelej.TreeCLI";
+$Version = "1.1.0.0";
+
+Write-Host "";
+$Env:NO_COLOR = "1"
+$Output = .\bin\ezstore.exe install $Id --ver $Version --verbosity m 2>&1;
+
+Write-Host "";
+if ( $Output -match '\x1b\[[0-9;]*m' ) {
+    Write-Host: "Output:";
+    Write-Host: $Output;
+    throw 'Output has colors.';
+}
+
+Write-Host "Test passed!";
+
--- tests/not-found.ps1
+++ tests/not-found.ps1
@@ -8,8 +8,6 @@ $Actual = .\bin\ezstore.exe install f1o2o3b4a5r6 --verbosity m 2>&1;
 $Actual = $Actual -replace '\x1b\[[0-9;]*m';
 
 Write-Host "";
-
-
 if ( $Actual -match $Expected ) {
     Write-Host "Test passed!";
     exit 0;
