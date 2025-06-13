d537e2127c9d8138f624a24f6938115eee13fe32
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-06-13 15:57:23 +0700

    Move install scenario

--- tests/install.ps1
+++ tests/install.ps1
@@ -1,1 +1,25 @@
-Write-Host "Install";
+Set-StrictMode -Version 3.0;
+$ErrorActionPreference = "Stop";
+trap { Write-Error $_ -ErrorAction Continue; exit 1 };
+
+$Id = "9mvsm3j7zj7c";
+$Name = "PeterEtelej.TreeCLI";
+$Version = "1.1.0.0";
+
+Write-Host "";
+.\bin\ezstore.exe install $Id --ver $Version --verbosity m
+
+Write-Host "";
+Import-Module -Name Appx -UseWindowsPowerShell -WarningAction SilentlyContinue;
+
+$Package = Get-AppxPackage -Name $Name;
+
+if ( $null -eq $Package ) {
+    throw "Package $Name not installed";
+}
+
+if ( $Package.Version -ne $Version ) {
+    throw "Wrong version installed. Expected $Version, actual: $($Package.Version)."
+}
+
+Write-Host "$Name ($Id) $Version successfully installed!";
