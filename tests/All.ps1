b4d84f201163debe28f92f1d7ccc027f710ee468
blbrdv <95272917+blbrdv@users.noreply.github.com>
2025-07-25 21:17:21 +0700

    Move utils to separate file

--- tests/All.ps1
+++ tests/All.ps1
@@ -9,90 +9,11 @@ param (
     [string] $Version
 )
 
+. "$PSScriptRoot\Utils.ps1"
+
 Set-StrictMode -Version 3.0;
 $ErrorActionPreference = "Stop";
 trap { Write-Output $_; exit 1 };
-
-function Exec-Ezstore {
-
-    param(
-        [Parameter(Mandatory=$true,Position=0)]
-        [string] $Path,
-        [Parameter(Mandatory=$true,Position=1)]
-        [string] $Arguments
-    )
-
-    $global:LASTEXITCODE = $null;
-
-    $Path = [IO.Path]::Combine((Get-Location).Path, $Path).Replace('\.','');
-
-    try {
-        Push-Location;
-        Set-Location $Path;
-
-        $Output = Invoke-Expression -Command ".\ezstore.exe $Arguments 2>&1";
-
-        $ExitCode = $global:LASTEXITCODE;
-        $global:LASTEXITCODE = $null;
-
-        return [string[]]($Output -split [Environment]::NewLine), [int]$ExitCode;
-    } finally {
-        Pop-Location;
-    }
-
-}
-
-function Install-Module-Force {
-
-    param(
-        [Parameter(Mandatory=$true)]
-        [string] $Name,
-        [AllowEmptyString()]
-        [string] $Version
-    )
-
-    if ( "" -eq $Version ) {
-        & {
-            $ProgressPreference = 'Ignore';
-            Install-Module -Name $Name -SkipPublisherCheck -Force 3>$null;
-        }
-    } else {
-        if ( $null -eq (Get-Module -ListAvailable -Name $Name | Where-object Version -ge $Version) ) {
-            & {
-                $ProgressPreference = 'Ignore';
-                Install-Module -Name $Name -MinimumVersion $Version -SkipPublisherCheck -Force 3>$null;
-            }
-        }
-    }
-
-
-}
-
-# See https://github.com/PowerShell/PowerShell/issues/13138#issuecomment-1820195503
-function Import-Module-Adhog {
-
-    param(
-        [Parameter(Mandatory=$true)]
-        [string] $Name,
-        [AllowEmptyString()]
-        [string] $Version
-    )
-
-    if ( "" -eq $Version ) {
-        & {
-            $ProgressPreference = 'Ignore';
-            Import-Module -Name $Name -UseWindowsPowerShell 3>$null;
-        }
-    } else {
-        if ( $null -eq (Get-Module -ListAvailable -Name $Name | Where-object Version -ge $Version) ) {
-            & {
-                $ProgressPreference = 'Ignore';
-                Import-Module -Name $Name -MinimumVersion $Version -UseWindowsPowerShell 3>$null;
-            }
-        }
-    }
-
-}
 
 $Targets = $Archs -split "," | ForEach-Object {
     @{
